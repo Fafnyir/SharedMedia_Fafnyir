f96afb65df212ef58a6d557b2b82e77e612460b8
Fafnyir <34715459+Fafnyir@users.noreply.github.com>
2024-08-15 16:01:22 -0400

    changes

--- FafnyirMedia.lua
+++ FafnyirMedia.lua
@@ -15,24 +15,11 @@ local LSM = LibStub("LibSharedMedia-3.0")
 --   FONT
 -- -----
 LSM:Register("font", "- The Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Regular.ttf]])
-LSM:Register("font", "- The Bold Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Bold.ttf]])
-LSM:Register("font", "- The Medium Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Medium.ttf]])
-LSM:Register("font", "- DAMAGE", [[Interface\Addons\FafnyirMedia\fonts\Browood-Regular.ttf]])
 LSM:Register("font", "- The Light Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Light.ttf]])
-LSM:Register("font", "- Gilroy", [[Interface\Addons\FafnyirMedia\fonts\Gilroy.ttf]])
-LSM:Register("font", "- LuckiestGuy", [[Interface\Addons\FafnyirMedia\fonts\LuckiestGuy.ttf]])
-LSM:Register("font", "- SigmarOne", [[Interface\Addons\FafnyirMedia\fonts\SigmarOne.ttf]])
-LSM:Register("font", "- BowlbyOne", [[Interface\Addons\FafnyirMedia\fonts\BowlbyOne.ttf]])
-LSM:Register("font", "- ProtestStrike", [[Interface\Addons\FafnyirMedia\fonts\ProtestStrike.ttf]])
-LSM:Register("font", "- GothamNarrowUltra", [[Interface\Addons\FafnyirMedia\fonts\GothamNarrowUltra.ttf]])
-LSM:Register("font", "- Catamaran-Black", [[Interface\Addons\FafnyirMedia\fonts\Catamaran-Black.ttf]])
-LSM:Register("font", "- Changa-ExtraBold", [[Interface\Addons\FafnyirMedia\fonts\Changa-ExtraBold.ttf]])
-LSM:Register("font", "- Raleway-Black", [[Interface\Addons\FafnyirMedia\fonts\Raleway-Black.ttf]])
-LSM:Register("font", "- SpaceGrotesk-Bold", [[Interface\Addons\FafnyirMedia\fonts\SpaceGrotesk-Bold.ttf]])
-LSM:Register("font", "- Urbanist-Black", [[Interface\Addons\FafnyirMedia\fonts\Urbanist-Black.ttf]])
-LSM:Register("font", "- TacOne", [[Interface\Addons\FafnyirMedia\fonts\TacOne.ttf]])
-LSM:Register("font", "- GasoekOne", [[Interface\Addons\FafnyirMedia\fonts\GasoekOne.ttf]])
-LSM:Register("font", "- RussoOne", [[Interface\Addons\FafnyirMedia\fonts\RussoOne.ttf]])
+LSM:Register("font", "- The Medium Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Medium.ttf]])
+LSM:Register("font", "- The Bold Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Bold.ttf]])
+LSM:Register("font", "- DAMAGE", [[Interface\Addons\FafnyirMedia\fonts\Browood-Regular.ttf]])
+
 
 --   SOUND
 -- -----
