e349355b7389d1cd62090d6bbfd4c7da1c7cdcdf
Fafnyir <34715459+Fafnyir@users.noreply.github.com>
2024-09-16 17:50:49 -0400

    UIpdate

--- FafnyirMedia.lua
+++ FafnyirMedia.lua
@@ -7,7 +7,7 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 -- ----- 
 --  MEDIA
---  Interface\Addons\FafnyirMedia\logo\LogoAddon.tga
+--  Interface\Addons\FafnyirMedia\logo\AddonLogo.tga
 -- ---- 
 
 
@@ -15,11 +15,10 @@ local LSM = LibStub("LibSharedMedia-3.0")
 --   FONT
 -- -----
 LSM:Register("font", "- The Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Regular.ttf]])
-LSM:Register("font", "- The Light Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Light.ttf]])
-LSM:Register("font", "- The Medium Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Medium.ttf]])
 LSM:Register("font", "- The Bold Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Bold.ttf]])
+LSM:Register("font", "- The Medium Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Medium.ttf]])
 LSM:Register("font", "- DAMAGE", [[Interface\Addons\FafnyirMedia\fonts\Browood-Regular.ttf]])
-
+LSM:Register("font", "- The Light Font", [[Interface\Addons\FafnyirMedia\fonts\Fafnyir-Light.ttf]])
 
 --   SOUND
 -- -----
@@ -27,8 +26,8 @@ LSM:Register("font", "- DAMAGE", [[Interface\Addons\FafnyirMedia\fonts\Browood-R
 -- -----
 --   STATUSBAR
 -- -----
-LSM:Register ("statusbar", "- Blizz Half", [[Interface\Addons\FafnyirMedia\statusbar\BlizzardOnePixelBar.tga]])
-LSM:Register ("statusbar", "- Blizz Half BG", [[Interface\Addons\FafnyirMedia\statusbar\BlizzardOnePixelBackground2.tga]])
 LSM:Register ("statusbar", "- Half", [[Interface\Addons\FafnyirMedia\statusbar\Half.tga]])
 LSM:Register ("statusbar", "- Gradient", [[Interface\Addons\FafnyirMedia\statusbar\Gradient.tga]])
+LSM:Register ("statusbar", "- Gradient Left", [[Interface\Addons\FafnyirMedia\statusbar\Gradient_L.tga]])
+LSM:Register ("statusbar", "- Gradient Right", [[Interface\Addons\FafnyirMedia\statusbar\Gradient_R.tga]])
 LSM:Register ("statusbar", "- Clean", [[Interface\Addons\FafnyirMedia\statusbar\Clean.tga]])
